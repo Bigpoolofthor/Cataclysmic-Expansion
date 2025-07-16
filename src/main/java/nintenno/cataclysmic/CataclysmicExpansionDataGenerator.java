121e6167c742c447f75d2fd3b144c67975dcc6c9
Bigpoolofthor <smartcampisikid@gmail.com>
2025-07-16 14:41:44 -0500

    Initial commit

---
+++ src/main/java/nintenno/cataclysmic/CataclysmicExpansionDataGenerator.java
@@ -0,0 +1,11 @@
+package nintenno.cataclysmic;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CataclysmicExpansionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
