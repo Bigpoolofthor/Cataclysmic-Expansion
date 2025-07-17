7ea79ad6e7fd49f18134949fbe4f9ed38f12a1c1
Bigpoolofthor <smartcampisikid@gmail.com>
2025-07-17 09:38:32 -0500

    Bug fixing item

--- src/main/java/nintenno/cataclysmic/item/ModItems.java
+++ src/main/java/nintenno/cataclysmic/item/ModItems.java
@@ -1,6 +1,5 @@
 package nintenno.cataclysmic.item;
 
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -11,27 +10,25 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import nintenno.cataclysmic.CataclysmicExpansion;
 
+import java.util.function.Function;
+
 public class ModItems {
 
-    public static final Item BRONZEINGOT = registerItem(new Item.Settings(),"bronzeingot");
+    public static final Item RUBY = registerItem("ruby", Item::new);
+    //Registers the items into the game
 
-    private static void addItemstoIngreadientGroup(FabricItemGroupEntries entries) {
-
-        entries.add(BRONZEINGOT);
+    private static Item registerItem(String name, Function<Item.Settings, Item> function) {
+        return Registry.register(Registries.ITEM, Identifier.of(CataclysmicExpansion.MOD_ID, name),
+                function.apply(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(CataclysmicExpansion.MOD_ID, name)))));
     }
-
-    private static Item registerItem(Item.Settings itemSettings, String name) {
-        Identifier id = Identifier.of(CataclysmicExpansion.MOD_ID);
-        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM,id);
-        Item.Settings settings = itemSettings.registryKey(key);
-
-        return Registry.register(Registries.ITEM, key, new Item(settings));
-
-    }
-
     public static void registerModItems() {
+        //Debug message when game is loading
         CataclysmicExpansion.LOGGER.info("Registering Mod Items for " + CataclysmicExpansion.MOD_ID);
+        //Item Group manager
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries ->
+            fabricItemGroupEntries.add(RUBY)
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemstoIngreadientGroup);
+        );
     }
+
 }
